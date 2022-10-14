853202e2dd664c90b76931df3e105c7280f0b76b
sherif <sherifahmedelsayed2@gmail.com>
2022-10-15 00:44:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,20 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
+
+# Sidebar header
 st.sidebar.header('Sampling Studio')
 
+# Browsing a file
 st.sidebar.subheader('Opened Chart')
 # time_hist_color = st.sidebar.selectbox('Color by') 
 
-st.sidebar.subheader('Created Sine Wave')
+st.sidebar.subheader('Generate Sine Wave')
 sine_amplitude = st.sidebar.slider('Amplitude', 0.0,1.0,1.0,0.01)
-sine_frequancy = st.sidebar.slider('Frequancy',0.5,100.0,10.0,0.1 )
+sine_frequancy = st.sidebar.slider('Frequancy',0.5,100.0,10.0,0.1)
 
-
-
-
-st.sidebar.subheader('SignaltoNoiseRatio')
-noise_flag = st.sidebar.checkbox("Add Noise",False)
+st.sidebar.subheader('Generate Noise')
+noise_flag = st.sidebar.checkbox("Add Noise To Signal",False)
 SNR = st.sidebar.slider('SNR% ', 1, 100, 1)
 
 st.sidebar.markdown('''
@@ -28,7 +27,7 @@ Created with ❤️ by SBME Students.
 ''')
 
 
-st.write("""### Opened ECG Signal""")
+st.write("""### ECG Signal""")
 if noise_flag:
     noisy_signal=createNoisySignal(SNR/100)
     st.line_chart(noisy_signal)
