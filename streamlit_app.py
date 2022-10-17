664b75557a586c82d78690fa555f09f5bac3f1f3
Esraa <esraa.ali021@eng-st.cu.edu.eg>
2022-10-17 20:27:35 +0200

    add browse section to side bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,104 @@
 import streamlit as st
 import sampling_studio_functions as functions
-from streamlit_option_menu import option_menu
+
 import streamlit.components.v1 as components
 
 
 result=(components.html("""
-  <input type="text" id="fname" name="fname" value="John"> 
+<style>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
+::selection{
+  color: #fff;
+  background: #6990F2;
+}
+.wrapper{
+  width: 550px;
+
+  border-radius: 5px;
+  padding: 30px;
+  box-shadow: 7px 7px 12px rgba(0,0,0,0.05);
+}
+.wrapper header{
+  color: #6990F2;
+  font-size: 27px;
+  font-weight: 600;
+  text-align: center;
+}
+.wrapper form{
+  height: 167px;
+  display: flex;
+  cursor: pointer;
+  margin: 30px 0;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border-radius: 5px;
+  border: 2px dashed #6990F2;
+}
+form :where(i, p){
+  color: #6990F2;
+}
+form i{
+  font-size: 50px;
+}
+form p{
+  margin-top: 15px;
+  font-size: 16px;
+}
+
+.custom-file-input::-webkit-file-upload-button {
+  visibility: hidden;
+}
+.custom-file-input::before {
+  content: 'Upload your signal';
+  display: inline-block;
+ background-color: #6990F2;
+  border: 1px solid #999;
+  border-radius: 3px;
+  padding: 12px 30px;
+  outline: none;
+  white-space: nowrap;
+  cursor: pointer;
+  font-weight: 700;
+  font-size: 10pt;
+  margin-top: 13px;
+  margin-left: 40px;
+}
+
+    </style>
+    <div class="wrapper">
+        <form action="#">
+        
+           <i class="fas fa-cloud-upload-alt"></i>
+           <input type="file" class="custom-file-input" name="filename">
+          </form>
+          
+        </form> 
+    </div>
   """))
 
 print(result)
 st.write(" ##   ")
 
 # # horizontal Menu
-selected = option_menu(None, ["Home", "Upload"], 
-    icons=['house', 'cloud-upload'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
+# selected = option_menu(None, ["Home", "Upload"], 
+#     icons=['house', 'cloud-upload'], 
+#     menu_icon="cast", default_index=0, orientation="horizontal")
     
-if selected == 'Upload':
-    uploaded_file = st.file_uploader("Choose a file")
+# if selected == 'Upload':
+#     uploaded_file = st.file_uploader("Choose a file")
 # st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+
 # Sidebar header
 st.sidebar.header('Sampling Studio')
-
+st.sidebar.file_uploader('')
 # Browsing a file
-st.sidebar.subheader('Opened Chart')
+
 # time_hist_color = st.sidebar.selectbox('Color by') 
 
 st.sidebar.subheader('Generate Sine Wave')
