104cf991330ea1706c879b1ce23367bda3ff4743
sherif <sherifahmedelsayed2@gmail.com>
2022-10-18 23:50:26 +0200

    adding and removing signals

--- sampling_studio_functions.py
+++ sampling_studio_functions.py
@@ -4,54 +4,57 @@ from scipy import interpolate
 from signal_class import Signal
 # import plotly_express as px
 
+# ------------------------ Variables --------------------------- #
+
 display_range=1000
 signal_data = pd.read_csv("ecg_data.csv")[:display_range]
 signal_time = signal_data["Time"]
 signal_amplitude = signal_data["Amplitude"]
-maximum_frequency = np.fft.fft(signal_data).max()
-sampling_frequency = 2 * maximum_frequency
 
-added_signals = signal_data.copy()
+# maximum_frequency = np.fft.fft(signal_data).max()
+# sampling_frequency = 2 * maximum_frequency
+
 added_signals_list = []
 
-def generateClearSignal():
-    return signal_data
 
+# ------------------------ Modifying Functions --------------------------- #
 
 def generateNoisySignal(SNR):
     SNR_db = 10*np.log10(SNR)
-    noisy_data = signal_data.copy()
-    power= noisy_data["Amplitude"] ** 2
+    power = signal_data["Amplitude"] ** 2
     signal_average_power= np.mean(power)
     signal_average_power_db = 10 * np.log10(signal_average_power)
     noise_db = signal_average_power_db - SNR_db
     noise_watts = 10 ** (noise_db/10)
 
-    noise = np.random.normal(0,np.sqrt(noise_watts),len(noisy_data))
-    
-    noisy_data["Amplitude"] += noise
-    return noisy_data
+    noise = np.random.normal(0,np.sqrt(noise_watts),len(signal_data))
+    noisy_data = signal_data["Amplitude"] + noise
+    return pd.DataFrame(noisy_data)
 
+# ------------------------------------------------------------------------ #
 
-def generateSineWave(amplitude, frequency):
-    time = np.arange(0,10,1/100)
-    sineWave = amplitude * np.sin(2 * np.pi * frequency * time)
-    sineWave_data = pd.DataFrame(sineWave,time)
-    return sineWave_data
+# def generateSineWave(amplitude, frequency):
+#     time = np.arange(0, 10, 1/100)
+#     sineWave = amplitude * np.sin(2 * np.pi * frequency * time)
+#     sine_wave_data = pd.DataFrame(sineWave, time)
+#     return sine_wave_data
 
+# ------------------------------------------------------------------------ #
 
-def addSignals(amplitude, frequency):
-    sineWave = amplitude * np.sin(2 * np.pi * frequency * signal_time)
-    added_signals_list.append(Signal(amplitude=amplitude,frequency=frequency))
-    # if noise_flag:
-    #     added_signals["Amplitude"] += (generateNoisySignal(SNR=SNR)["Amplitude"] + sineWave)
-    # else:
-    added_signals["Amplitude"] += sineWave
-    print(len(added_signals_list))
-    return added_signals
+def drawAddedSignals(noise_flag, SNR = 100):
+    clear_added_signal = signal_data.copy()
+    noisy_added_signal = generateNoisySignal(SNR = SNR)
+    for signal in added_signals_list:
+        clear_added_signal["Amplitude"] += (signal.amplitude * np.sin(2 * np.pi * signal.frequency * signal_time))
+        noisy_added_signal["Amplitude"] += (signal.amplitude * np.sin(2 * np.pi * signal.frequency * signal_time))
+    if noise_flag:
+        return noisy_added_signal
+    else:
+        return clear_added_signal
+
+# ------------------------------------------------------------------------ #
 
 def generateSampledSignal(sampling_frequency):
-
     func = interpolate.interp1d(signal_data["Time"], signal_data["Amplitude"],kind='quadratic')
     time = np.arange(0,0.5,1/sampling_frequency)
     ynew = func(time)
@@ -60,8 +63,25 @@ def generateSampledSignal(sampling_frequency):
     # sampled_signal = signal_data[:-1:round(Ts*1000)]
     return pd.DataFrame(ynew,time)
 
+# ------------------------------------------------------------------------ #
 
-def removeSignals(signal):
-    removed_signal = signal.amplitude * np.sin(2 * np.pi * signal.frequency * signal_time)
-    added_signals_list.remove(signal)
-    added_signals["Amplitude"] -= removed_signal
+def addSignal(amplitude, frequency):
+    added_signals_list.append(Signal(amplitude = amplitude, frequency = frequency))
+
+# ------------------------------------------------------------------------ #
+
+def removeSignal(amplitude,frequency):
+    for added_signal in added_signals_list:
+        if added_signal.amplitude == amplitude and added_signal.frequency == frequency:
+            added_signals_list.remove(added_signal)
+            return
+
+# ---------------------------- Getter functions -------------------------- #
+
+def getClearSignal():
+    return signal_data
+
+# ------------------------------------------------------------------------ #
+
+def getAddedSignalsList():
+    return added_signals_list
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,49 @@
 import streamlit as st
 import pandas as pd
-import os
 import sampling_studio_functions as func
+from signal_class import Signal
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# # # Sidebar Elements
+# ---------------------- Sidebar Elements -------------------------------- #
 
 # # Sidebar header
 website_title = '<p class="page_titel">Sampling Studio</p>'
-st.sidebar.markdown(website_title, unsafe_allow_html=True)
+st.sidebar.markdown(website_title, unsafe_allow_html = True)
 # st.sidebar.header('Sampling Studio')
 
+# ------------------------------------------------------------------------ #
+
 # # Browsing a file
-uploaded_file = st.sidebar.file_uploader(
-    "", type="csv", accept_multiple_files=False)
+uploaded_file = st.sidebar.file_uploader("", type="csv", accept_multiple_files = False)
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write(df)
 
+# ------------------------------------------------------------------------ #
 
 # # line break
 st.sidebar.markdown("***")
 
-# time_hist_color = st.sidebar.selectbox('Color by')
+# ------------------------------------------------------------------------ #
 
-# # Add signals to the origin signal
+# # Add noise to signal
+st.sidebar.header('Noise')
+noise_flag = st.sidebar.checkbox("Add Noise", False)
+if noise_flag:
+    SNR_slider_value = st.sidebar.slider('SNR%', 1, 100, 50)
+else:
+    SNR_slider_value = 1
+
+# ------------------------------------------------------------------------ #
+
+# # line break
+st.sidebar.markdown("***")
+
+# ------------------------------------------------------------------------ #
+
+# # Add signals to the original signal
 st.sidebar.header('Add Signals')
 
 adding_col1, adding_col2 = st.sidebar.columns(2)
@@ -37,34 +54,38 @@ with adding_col1:
 with adding_col2:
     sine_frequancy = st.slider('Frequency', 0.5, 20.0, 10.0, 0.1)
 
-
 add_signal_button = st.sidebar.button("Add Signal...", key="add")
-
 if add_signal_button:
-    func.addSignals(sine_amplitude, sine_frequancy)
+    func.addSignal(sine_amplitude, sine_frequancy)
+
+# ------------------------------------------------------------------------ #
+
+# # Show every signal amplitude and frequency in a select box
 Options = []
-# # Show every signal amplitude and frequency (last signal will be deleted if you click the button twice)
-for signal in func.added_signals_list:
+for signal in func.getAddedSignalsList():
     Options.append(f"Amp: {signal.amplitude},F: {signal.frequency}")
 selected_signal = st.sidebar.selectbox("Signals", Options)
+selected_signal_arr = str(selected_signal).split(",")
+if len(selected_signal_arr) != 1:
+    amplitude_sub = float(selected_signal_arr[0][4:])
+    frequency_sub = float(selected_signal_arr[1][2:])
+
 remove_signal_button = st.sidebar.button("Remove")
-# # line break
-st.sidebar.markdown("***")
+if remove_signal_button and len(func.getAddedSignalsList()) > 0:
+    func.removeSignal(amplitude=amplitude_sub, frequency=frequency_sub)
 
-# # Add noise to signal
-st.sidebar.header('Noise')
-noise_flag = st.sidebar.checkbox("Add Noise", False)
-if noise_flag:
-    SNR = st.sidebar.slider('SNR%', 1, 100, 50)
-else:
-    SNR = 0
+# ------------------------------------------------------------------------ #
 
 # # line break
 st.sidebar.markdown("***")
+
+# ------------------------------------------------------------------------ #
 
 # # Sampling
 st.sidebar.header('Sampling')
 sampling_rate = st.sidebar.slider('Sampling Frequency (Fs)', 10, 500, 100)
+
+# ------------------------------------------------------------------------ #
 
 # # Sidebar bottom
 st.sidebar.markdown('''
@@ -72,28 +93,37 @@ st.sidebar.markdown('''
 Created with ❤️ by SBME Students
 ''')
 
-# # # Main Window Elements
+# ----------------------- Main Window Elements --------------------------- #
 
 st.write("""### Signal""")
 if noise_flag:
-    noisy_signal = func.generateNoisySignal(SNR)
-    st.line_chart(noisy_signal, x='Time', y='Amplitude')
+    noisy_signal = func.generateNoisySignal(SNR = SNR_slider_value)
+    st.line_chart(noisy_signal)
 else:
-    signal = func.generateClearSignal()
-    st.line_chart(signal, x='Time', y='Amplitude')
+    st.line_chart(func.getClearSignal(), x='Time', y='Amplitude')
+
+# ------------------------------------------------------------------------ #
 
 # # line break
 st.markdown("***")
+
+# ------------------------------------------------------------------------ #
 
 # st.write("""### Generated Sine Wave""")
 # generated_sine = functions.generateSineWave(sine_amplitude, sine_frequancy)
 # st.line_chart(generated_sine)
 
+# ------------------------------------------------------------------------ #
+
 st.write("""### Resulted Signal""")
-st.line_chart(func.added_signals, x='Time', y='Amplitude')
+st.line_chart(func.drawAddedSignals(noise_flag=noise_flag,SNR=SNR_slider_value),x="Time",y="Amplitude")
+
+# ------------------------------------------------------------------------ #
 
 # # line break
 st.markdown("***")
+
+# ------------------------------------------------------------------------ #
 
 st.write("""### Reconstructed Signal""")
 # print(functions.generateSampledSignal(fs))
