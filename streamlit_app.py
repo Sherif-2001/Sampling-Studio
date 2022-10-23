63e01810db6845089b358dc1e9dedbd87ef64ad8
Abram1111 <ebram.meawad01@eng-st.cu.edu.eg>
2022-10-23 18:58:06 +0200

    new styling

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,74 +12,61 @@ with open('style.css') as f:
 file_as_array = None
 uploaded_file = None
 # ---------------------- Sidebar Elements -------------------------------- #
-# Sidebar header
-website_title = '<p class="page_titel">Sampling Studio</p>'
-st.markdown(website_title, unsafe_allow_html = True)
 
 # ------------------------------------------------------------------------ #
 
 # # Browsing a file
-st.sidebar.markdown("## Upload Signal")
-file = st.sidebar.file_uploader("", type="csv", accept_multiple_files = False)
+label_col, Button_col = st.sidebar.columns(2)
+with label_col:
+    st.markdown("## Upload Signal")
+with Button_col:
+    file = st.file_uploader(
+        "", type="csv", accept_multiple_files=False)
+
 uploaded_file = file
 if file is not None:
     file_as_data_frame = pd.read_csv(file).values[:1000]
-    file_as_flat_list = [item for sublist in file_as_data_frame for item in sublist]
+    file_as_flat_list = [
+        item for sublist in file_as_data_frame for item in sublist]
     file_as_array = np.asarray(file_as_flat_list)
 
-# ------------------------------------------------------------------------ #
-st.sidebar.markdown("***")
-# ------------------------------------------------------------------------ #
-
-# st.sidebar.markdown("## Generate Signal")
-# slider1 ,slider2 = st.sidebar.columns(2)
-# with slider1:
-#     amplitude_num_input = st.number_input("Amplitude",0.0, 1.0, 1.0, 0.05,key="default_amp_slider",)
-# with slider2:
-#     frequency_num_input = st.number_input("Frequency", 0.5, 20.0, 10.0, 1.0,key="default_freq_slider")
-# phase_num_input = st.sidebar.number_input("Phase", 0, 360, 0,key="default_phase_slider")
-# generate_button = st.sidebar.button("Generate...")
-# if generate_button and file is None:
-#     func.setGeneratedSignal(amplitude_num_input,frequency_num_input,phase_num_input)
-# ------------------------------------------------------------------------ #
-
-# # line break
-# st.sidebar.markdown("***")
-# ------------------------------------------------------------------------ #
 
 # # Add noise to signal
-st.sidebar.header('Noise')
-noise_flag = st.sidebar.checkbox("Add Noise", False)
-SNR_slider_value = st.sidebar.slider('SNR', 1, 100, 50)
-# ------------------------------------------------------------------------ #
-
-# # line break
-st.sidebar.markdown("***")
-# ------------------------------------------------------------------------ #
-
+address_col, check_col = st.sidebar.columns(2)
+with address_col:
+    st.header('Noise')
+with check_col:
+    noise_flag = st.checkbox("", False)
+if noise_flag:
+    SNR_slider_value = st.sidebar.slider('SNR', 1, 100, 50)
+else:
+    SNR_slider_value = 1
 # # Add signals to the original signal
 st.sidebar.header('Add Signals')
 
-signal_amplitude_slider_col1, signal_frequency_slider_col2, signal_phase_slider_col3 = st.sidebar.columns(3)
+adding_signal_slider_col1, adding_signal_slider_col2 = st.sidebar.columns(2)
 
-with signal_amplitude_slider_col1:
-    signal_amplitude_slider = st.slider('Amplitude', 0.0, 1.0, 1.0, 0.01,format="%f")
+with adding_signal_slider_col1:
+    added_signal_amplitude = st.slider('Amplitude', 0.0, 1.0, 1.0, 0.01)
 
-with signal_frequency_slider_col2:
-    signal_frequancy_slider = st.slider('Frequency', 0.5, 20.0, 10.0, 0.1,format="%f")
+with adding_signal_slider_col2:
+    added_signal_frequancy = st.slider('Frequency', 0.5, 20.0, 10.0, 0.1)
 
-with signal_phase_slider_col3:
-    signal_phase_slider = st.slider('Phase', 0.0, 2.0, 0.0, 0.1,format="%fπ")
+
+added_signal_phase = st.sidebar.slider('Phase', 0, 360, 0)
+
 
 add_signal_button = st.sidebar.button("Add Signal...", key="add")
 if add_signal_button:
-    func.addSignalToList(signal_amplitude_slider, signal_frequancy_slider, signal_phase_slider)
+    func.addSignalToList(added_signal_amplitude,
+                         added_signal_frequancy, added_signal_phase)
 # ------------------------------------------------------------------------ #
 
 # # Show every signal amplitude and frequency in a select box
 selectbox_signals_list = []
 for signal in func.getAddedSignalsList():
-    selectbox_signals_list.append(f"Amp: {signal.amplitude} / Freq: {signal.frequency} / Phase: {round(signal.phase * np.pi)}".format())
+    selectbox_signals_list.append(
+        f"Amp: {signal.amplitude} / Freq: {signal.frequency} / Phase: {round(signal.phase * np.pi)}".format())
 selected_signal = st.sidebar.selectbox("Signals", selectbox_signals_list)
 selected_signal_split = str(selected_signal).split(" ")
 if len(selected_signal_split) != 1:
@@ -92,7 +79,8 @@ remove_button_col, clear_button_col = st.sidebar.columns(2)
 with remove_button_col:
     remove_signal_button = st.button("Remove")
     if remove_signal_button and len(func.getAddedSignalsList()) > 0:
-        func.removeSignalFromList(amplitude=amplitude_sub, frequency=frequency_sub,phase=phase_sub)
+        func.removeSignalFromList(
+            amplitude=amplitude_sub, frequency=frequency_sub, phase=phase_sub)
         st.experimental_rerun()
 
 
@@ -104,54 +92,40 @@ with clear_button_col:
 
 # ------------------------------------------------------------------------ #
 
-# # line break
-st.sidebar.markdown("***")
-# ------------------------------------------------------------------------ #
 
 # # Sampling
 st.sidebar.header('Sampling')
-sampling_rate = st.sidebar.slider('Factor Fs/Fmax', 0.5, 10.0,2.0,0.5,format="%f")
+sampling_rate = st.sidebar.slider(
+    'Factor Fs/Fmax', 0.5, 10.0, 2.0, 0.5, format="%f")
 
 # ------------------------------------------------------------------------ #
 
 # # Sidebar bottom
 st.sidebar.markdown('''
----
 ©2022 SBME All rights reserved.
 ''')
 # ----------------------- Main Window Elements --------------------------- #
 
-# if uploaded_file is not None:
-#     st.write("""### Uploaded Signal""")
-#     st.line_chart(pd.DataFrame(file_as_array,func.getTime()))
-# else:
-#     st.write("""### Generated Signal""")
-
-#     st.line_chart(func.getSignalData(uploaded_file))
-
-
-# st.write("""### Preview Sine Wave""")
-# generated_sine = func.renderGeneratedSignal(added_signal_amplitude, added_signal_frequancy, added_signal_phase)
-# st.line_chart(generated_sine)
-# ------------------------------------------------------------------------ #
-
-# st.write("""### Resulted Signal""")
+website_title = '<p class="page_titel">Sampling Studio</p>'
+st.markdown(website_title, unsafe_allow_html=True)
 func.renderResultedSignal(noise_flag, file_as_array, SNR_slider_value)
 # ------------------------------------------------------------------------ #
 
 # st.write("""### Reconstructed Signal""")
-fig,Reconstructed_signal = func.renderSampledSignal(sampling_rate)
+fig, Reconstructed_signal = func.renderSampledSignal(sampling_rate)
 fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
 fig.update_traces(line_color='#EAE3FF')
 
 
-st.plotly_chart(fig,use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
 # ------------------------------------------------------------------------ #
 
+
 @st.cache
-def convert_df(downloaded_df): 
+def convert_df(downloaded_df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return downloaded_df.to_csv().encode('utf-8')
+
 
 csv = convert_df(Reconstructed_signal)
 
